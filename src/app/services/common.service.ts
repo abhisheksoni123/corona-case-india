c679463d117827086df4974508c1513c740c5bf5
abhisheksoni123 <abhishek.chandrasoni@hashtaag.com>
2020-08-22 23:28:55 +0530

    commit with graph implemented

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-//import { Subject} from "rxjs/Subject";
-//import 'rxjs/add/operator/map';
 import { map } from "rxjs/operators";
 
 @Injectable({
@@ -15,5 +13,4 @@ export class CommonBaseService {
     return this.http.get("https://api.covid19india.org/v4/timeseries.json")
     .pipe(map(res => res));
   }
-  
 }
